d98731ed67dc5e1698c0a5f110621ed6279a8862
Jan David <jdno@jdno.dev>
2022-03-05 12:07:33 +0100

    Extract screen size into constants

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ use bevy::prelude::*;
 
 mod map;
 
+/// The width of the game's window
+const SCREEN_WIDTH: f32 = 1024.0;
+
+/// The height of the game's window
+const SCREEN_HEIGHT: f32 = 768.0;
+
 fn main() {
     App::new()
         // Must be added before the DefaultPlugins
         .insert_resource(WindowDescriptor {
             title: "Auto Traffic Control".to_string(),
-            width: 1024.0,
-            height: 768.0,
+            width: SCREEN_WIDTH,
+            height: SCREEN_HEIGHT,
+            resizable: false,
             ..Default::default()
         })
         .insert_resource(ClearColor(Color::BLACK))
