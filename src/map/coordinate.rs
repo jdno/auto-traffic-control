6c8ccf6dcc12cc8c3f83afa7765d4fb47a9a7449
Jan David <jdno@jdno.dev>
2022-03-07 10:36:11 +0100

    Temporarily silence dead code warning

--- src/map/coordinate.rs
+++ src/map/coordinate.rs
@@ -21,6 +21,7 @@ pub struct Coordinate {
 }
 
 impl Coordinate {
+    #[cfg(test)] // Constructor is currently only used in tests
     pub fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
