0afe98501f25ba6ca8d218bd33d2485f7bc60490
Jan David <jdno@jdno.dev>
2022-03-08 16:55:37 +0100

    Spawn airplane at random location around the map

--- src/systems/spawn_airplane.rs
+++ src/systems/spawn_airplane.rs
@@ -1,10 +1,11 @@
 use bevy::prelude::*;
+use rand::Rng;
 
 use crate::components::{Airplane, FlightPlan, Speed};
-use crate::map::{route_between, Tile, MAP_WIDTH_RANGE};
+use crate::map::{route_between, Tile, MAP_HEIGHT_RANGE, MAP_WIDTH_RANGE};
 
 pub fn spawn_airplane(mut commands: Commands) {
-    let spawn = Tile::new(*MAP_WIDTH_RANGE.start(), 0);
+    let spawn = random_spawn();
     let spawn_point = spawn.as_point();
 
     let airport = Tile::new(0, 0);
@@ -26,4 +27,27 @@ pub fn spawn_airplane(mut commands: Commands) {
         .insert(Airplane)
         .insert(FlightPlan::new(flight_plan))
         .insert(Speed::new(32.0));
+}
+
+fn random_spawn() -> Tile {
+    let mut rng = rand::thread_rng();
+
+    match rng.gen_range(0u32..4u32) {
+        0 => {
+            let x = rng.gen_range(MAP_WIDTH_RANGE);
+            Tile::new(x, *MAP_HEIGHT_RANGE.end())
+        }
+        1 => {
+            let y = rng.gen_range(MAP_HEIGHT_RANGE);
+            Tile::new(*MAP_WIDTH_RANGE.end(), y)
+        }
+        2 => {
+            let x = rng.gen_range(MAP_WIDTH_RANGE);
+            Tile::new(x, *MAP_HEIGHT_RANGE.start())
+        }
+        _ => {
+            let y = rng.gen_range(MAP_HEIGHT_RANGE);
+            Tile::new(*MAP_WIDTH_RANGE.start(), y)
+        }
+    }
 }
