8ae4b38af08516426b570c64b5618d6e51087e22
Jan David <jdno@jdno.dev>
2022-03-10 14:49:03 +0100

    Create generator for airplane ids

--- src/atc-game/src/components/airplane_id.rs
+++ src/atc-game/src/components/airplane_id.rs
@@ -6,10 +6,6 @@ use crate::api::IntoApi;
 pub struct AirplaneId(String);
 
 impl AirplaneId {
-    pub fn new(id: String) -> Self {
-        Self(id)
-    }
-
     pub fn get(&self) -> &str {
         &self.0
     }
@@ -23,24 +19,49 @@ impl IntoApi for AirplaneId {
     }
 }
 
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+pub struct AirplaneIdGenerator {
+    last_id: u32,
+}
+
+impl AirplaneIdGenerator {
+    pub fn new() -> Self {
+        Self { last_id: 0 }
+    }
+
+    pub fn generate(&mut self) -> AirplaneId {
+        self.last_id += 1;
+
+        AirplaneId(format!("AT-{:0width$}", self.last_id, width = 4))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::api::IntoApi;
 
-    use super::AirplaneId;
+    use super::{AirplaneId, AirplaneIdGenerator};
 
     #[test]
     fn get() {
         let id = "test";
-        let airplane_id = AirplaneId::new(String::from(id));
+        let airplane_id = AirplaneId(String::from(id));
 
         assert_eq!(id, airplane_id.get());
     }
 
     #[test]
+    fn generate() {
+        let mut generator = AirplaneIdGenerator::new();
+
+        assert_eq!("AT-0001", generator.generate().get());
+        assert_eq!("AT-0002", generator.generate().get());
+    }
+
+    #[test]
     fn trait_into_api() {
         let id = String::from("test");
-        let airplane_id = AirplaneId::new(id.clone());
+        let airplane_id = AirplaneId(id.clone());
 
         assert_eq!(id, airplane_id.into_api());
     }
