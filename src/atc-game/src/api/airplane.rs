1156ff3bbacbd385850c0519b5a76b3d42e2ba9d
Jan David <jdno@jdno.dev>
2022-03-11 14:43:39 +0100

    Mock update flight plan endpoint

--- src/atc-game/src/api/airplane.rs
+++ src/atc-game/src/api/airplane.rs
@@ -2,9 +2,13 @@ use std::sync::Arc;
 
 use tonic::{Request, Response, Status};
 
-use atc::v1::{GetAirplaneRequest, GetAirplaneResponse};
+use atc::v1::{
+    GetAirplaneRequest, GetAirplaneResponse, UpdateFlightPlanRequest, UpdateFlightPlanResponse,
+};
 
 use crate::command::CommandSender;
+use crate::components::{FlightPlan, ValidationError};
+use crate::map::Tile;
 use crate::store::Store;
 
 #[derive(Clone, Debug)]
@@ -31,6 +35,52 @@ impl atc::v1::airplane_service_server::AirplaneService for AirplaneService {
         if let Some(airplane) = self.store.get(&id) {
             Ok(Response::new(GetAirplaneResponse {
                 airplane: Some(airplane.clone()),
+            }))
+        } else {
+            Err(Status::not_found(&format!(
+                "No airplane with id {id} was found"
+            )))
+        }
+    }
+
+    async fn update_flight_plan(
+        &self,
+        request: Request<UpdateFlightPlanRequest>,
+    ) -> Result<Response<UpdateFlightPlanResponse>, Status> {
+        let request = request.into_inner();
+        let id = request.id;
+
+        if let Some(_airplane) = self.store.get(&id) {
+            let new_flight_plan = request.flight_plan;
+            let tiles = new_flight_plan
+                .iter()
+                .map(|node| Tile::new(node.x, node.y))
+                .collect();
+
+            let errors = match FlightPlan::new(tiles) {
+                Ok(_flight_plan) => {
+                    // TODO: Queue a command on the command bus
+                    Vec::new()
+                }
+                Err(errors) => errors
+                    .iter()
+                    .map(|error| match error {
+                        ValidationError::HasSharpTurns => {
+                            atc::v1::update_flight_plan_response::Error::HasSharpTurns.into()
+                        }
+                        ValidationError::NodeOutOfBounds => {
+                            atc::v1::update_flight_plan_response::Error::NodeOutOfBounds.into()
+                        }
+                        ValidationError::NotInLogicalOrder => {
+                            atc::v1::update_flight_plan_response::Error::NotInLogicalOrder.into()
+                        }
+                    })
+                    .collect(),
+            };
+
+            // TODO: Create different responses for successful and failed updates
+            Ok(Response::new(UpdateFlightPlanResponse {
+                validation_errors: errors,
             }))
         } else {
             Err(Status::not_found(&format!(
