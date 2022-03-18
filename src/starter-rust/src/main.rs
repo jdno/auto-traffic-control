848623baedcd0de4d588a8814f60ca7abdd9b6bf
Jan David <jdno@jdno.dev>
2022-03-18 10:23:57 +0100

    Subscribe to event service

--- src/starter-rust/src/main.rs
+++ src/starter-rust/src/main.rs
@@ -1,5 +1,19 @@
+use tokio_stream::StreamExt;
+
+use atc::v1::event_service_client::EventServiceClient;
+use atc::v1::StreamRequest;
+
 mod route;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = EventServiceClient::connect("http://localhost:4747").await?;
+
+    let mut stream = client.stream(StreamRequest {}).await.unwrap().into_inner();
+
+    while let Some(message) = stream.next().await {
+        let _event = message.unwrap().event.unwrap();
+    }
+
+    Ok(())
 }
