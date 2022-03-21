62bcb7b37d32392caa9f96995cb2ddc8ffd5830c
Jan David <jdno@jdno.dev>
2022-03-21 15:03:07 +0100

    Create resource for map

--- game/src/map/mod.rs
+++ game/src/map/mod.rs
@@ -1,5 +1,8 @@
 use std::ops::RangeInclusive;
 
+use atc::v1::Map as ApiMap;
+
+use crate::api::AsApi;
 use crate::{SCREEN_HEIGHT, SCREEN_WIDTH, TILE_SIZE};
 
 pub use self::direction::*;
@@ -21,3 +24,41 @@ pub const MAP_HEIGHT_RANGE: RangeInclusive<i32> =
 pub const MAP_WIDTH: usize = (SCREEN_WIDTH as i32 / TILE_SIZE) as usize - (BORDER_SIZE * 2) - 1;
 
 pub const MAP_WIDTH_RANGE: RangeInclusive<i32> = -(MAP_WIDTH as i32 / 2)..=(MAP_WIDTH as i32 / 2);
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+pub struct Map {
+    airport: Node,
+}
+
+impl Map {
+    pub fn new(airport: Node) -> Self {
+        Self { airport }
+    }
+}
+
+impl AsApi for Map {
+    type ApiType = ApiMap;
+
+    fn as_api(&self) -> Self::ApiType {
+        ApiMap {
+            airport: Some(self.airport.as_api()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Map;
+
+    #[test]
+    fn trait_send() {
+        fn assert_send<T: Send>() {}
+        assert_send::<Map>();
+    }
+
+    #[test]
+    fn trait_sync() {
+        fn assert_sync<T: Sync>() {}
+        assert_sync::<Map>();
+    }
+}
--- game/src/state/running.rs
+++ game/src/state/running.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 use atc::v1::get_game_state_response::GameState;
 
 use crate::event::{Event, EventBus};
+use crate::map::{Map, Node};
 use crate::systems::{
     detect_collision, follow_flight_plan, generate_flight_plan, setup_airport, setup_grid,
     spawn_airplane, update_flight_plan, SpawnTimer,
@@ -13,6 +14,7 @@ pub struct GameStateRunningPlugin;
 impl Plugin for GameStateRunningPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(SpawnTimer::new(Timer::from_seconds(1.0, true)))
+            .insert_resource(Map::new(Node::new(0, 0)))
             .add_system_set(
                 SystemSet::on_enter(GameState::Running)
                     .with_system(send_event)
