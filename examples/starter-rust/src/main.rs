302bc3d82f1713c7d04ae4a6eb0a860a61d0c94f
Jan David <jdno@jdno.dev>
2022-03-24 19:56:14 +0100

    Prototype path finding in Rust bot (#48)
    The proof-of-concept Rust bot has been rewritten to use path finding to
    route airplanes to the airport. The bot also supports multiple airports
    and tags now. Path finding is done with an external crate and the A*
    algorithm.

--- examples/starter-rust/src/main.rs
+++ examples/starter-rust/src/main.rs
@@ -1,9 +1,12 @@
+use std::ops::Deref;
+use std::rc::Rc;
 use std::thread::sleep;
 use std::time::Duration;
+
+use pathfinding::prelude::astar;
 use tokio_stream::StreamExt;
 use tonic::transport::Channel;
 
-use crate::route::route_between;
 use atc::v1::airplane_service_client::AirplaneServiceClient;
 use atc::v1::event_service_client::EventServiceClient;
 use atc::v1::game_service_client::GameServiceClient;
@@ -14,7 +17,11 @@ use atc::v1::{
     UpdateFlightPlanRequest,
 };
 
-mod route;
+use crate::map::{Airport, Map};
+use crate::point::{Coord, Point};
+
+mod map;
+mod point;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -39,12 +46,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         start_game(&mut game_service).await;
     }
 
+    let mut map = Rc::new(None);
+
     while let Some(message) = stream.next().await {
         let event = message.unwrap().event.unwrap();
 
         match event {
             Event::AirplaneDetected(airplane_detected) => {
-                create_flight_plan(&mut airplane_service, airplane_detected).await;
+                create_flight_plan(&mut airplane_service, airplane_detected, &map).await;
+            }
+            Event::GameStarted(game_started) => {
+                let game_map = game_started.map.unwrap();
+
+                let airports = game_map
+                    .airports
+                    .iter()
+                    .map(|airport| {
+                        let node = airport.node.as_ref().unwrap();
+
+                        Airport {
+                            coord: Coord(node.longitude, node.latitude, false),
+                            tag: airport.tag(),
+                        }
+                    })
+                    .collect();
+
+                let routing_grid = game_map
+                    .routing_grid
+                    .iter()
+                    .map(|node| Coord(node.longitude, node.latitude, node.restricted))
+                    .collect();
+
+                map = Rc::new(Some(Map {
+                    airports,
+                    routing_grid,
+                    width: game_map.width,
+                    height: game_map.height,
+                }));
             }
             Event::GameStopped(game_stopped) => {
                 let points = game_stopped.score;
@@ -68,6 +106,7 @@ async fn start_game(game_service: &mut GameServiceClient<Channel>) {
 async fn create_flight_plan(
     airplane_service: &mut AirplaneServiceClient<Channel>,
     airplane_detected: AirplaneDetected,
+    map: &Rc<Option<Map>>,
 ) {
     let airplane = airplane_detected.airplane.unwrap();
     let first_hop = airplane
@@ -75,12 +114,44 @@ async fn create_flight_plan(
         .get(0)
         .expect("expected flight plan to have at least one hop");
 
-    let destination = Node {
-        longitude: 0,
-        latitude: 0,
-        restricted: false,
+    let starting_point = Point {
+        x: first_hop.longitude,
+        y: first_hop.latitude,
+        map: map.clone(),
     };
-    let route = route_between(first_hop, &destination, true);
+
+    let airport = &map
+        .deref()
+        .as_ref()
+        .unwrap()
+        .airports
+        .iter()
+        .find(|airport| airport.tag == airplane.tag())
+        .unwrap();
+
+    let destination_point = Point {
+        x: airport.coord.0,
+        y: airport.coord.1,
+        map: map.clone(),
+    };
+
+    let route = astar(
+        &starting_point,
+        |p| p.neighbors(),
+        |p| p.distance(&destination_point) / 3,
+        |p| p == &destination_point,
+    )
+    .unwrap()
+    .0;
+
+    let route = route
+        .iter()
+        .map(|point| Node {
+            longitude: point.x,
+            latitude: point.y,
+            restricted: false,
+        })
+        .collect();
 
     let request = UpdateFlightPlanRequest {
         id: airplane.id,
---
+++ examples/starter-rust/src/point.rs
@@ -0,0 +1,99 @@
+use std::ops::Deref;
+use std::rc::Rc;
+
+use pathfinding::prelude::*;
+
+use crate::map::Map;
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+pub struct Coord(pub i32, pub i32, pub bool);
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+    pub map: Rc<Option<Map>>,
+}
+
+impl Point {
+    pub fn distance(&self, other: &Point) -> u32 {
+        (absdiff(self.x, other.x) + absdiff(self.y, other.y)) as u32
+    }
+
+    pub fn neighbors(&self) -> Vec<(Point, u32)> {
+        let potential_neighbors = vec![
+            (
+                Point {
+                    x: self.x,
+                    y: self.y + 1,
+                    map: self.map.clone(),
+                },
+                1,
+            ),
+            (
+                Point {
+                    x: self.x + 1,
+                    y: self.y + 1,
+                    map: self.map.clone(),
+                },
+                2,
+            ),
+            (
+                Point {
+                    x: self.x + 1,
+                    y: self.y,
+                    map: self.map.clone(),
+                },
+                1,
+            ),
+            (
+                Point {
+                    x: self.x + 1,
+                    y: self.y - 1,
+                    map: self.map.clone(),
+                },
+                2,
+            ),
+            (
+                Point {
+                    x: self.x,
+                    y: self.y - 1,
+                    map: self.map.clone(),
+                },
+                1,
+            ),
+            (
+                Point {
+                    x: self.x - 1,
+                    y: self.y - 1,
+                    map: self.map.clone(),
+                },
+                2,
+            ),
+            (
+                Point {
+                    x: self.x - 1,
+                    y: self.y,
+                    map: self.map.clone(),
+                },
+                1,
+            ),
+            (
+                Point {
+                    x: self.x - 1,
+                    y: self.y + 1,
+                    map: self.map.clone(),
+                },
+                2,
+            ),
+        ];
+
+        let map: &Map = self.map.deref().as_ref().unwrap();
+
+        potential_neighbors
+            .into_iter()
+            .filter(|(point, _)| map.is_within_bounds(point))
+            .filter(|(point, _)| !map.coord_at(point.x, point.y).2)
+            .collect()
+    }
+}
