87eee9191c54845dce6dda279a8981908dfcf885
Jan David <jdno@jdno.dev>
2022-03-24 12:09:15 +0100

    Add go-around procedure for airplanes with the wrong tag

--- game/src/systems/despawn_airplane.rs
+++ game/src/systems/despawn_airplane.rs
@@ -1,21 +1,28 @@
 use bevy::prelude::*;
 
-use crate::components::{AirplaneId, Landing};
+use crate::components::{AirplaneId, FlightPlan, Landing, Tag};
 use crate::event::{Event, EventBus};
-use crate::map::Map;
+use crate::map::{Airport, Map, Node};
 use crate::resources::Score;
 
 pub fn despawn_airplane(
     mut commands: Commands,
     map: Res<Map>,
     mut score: ResMut<Score>,
-    query: Query<(Entity, &AirplaneId, &Transform), With<Landing>>,
+    mut query: Query<(Entity, &AirplaneId, &mut FlightPlan, &Tag, &Transform), With<Landing>>,
     event_bus: Local<EventBus>,
 ) {
-    let airport = map.airport().node().as_vec3(2.0);
+    let airport_location = map.airport().node().as_vec3(2.0);
+    let airport_tag = map.airport().tag();
 
-    for (entity, airplane_id, transform) in query.iter() {
-        if transform.translation == airport {
+    for (entity, airplane_id, mut flight_plan, tag, transform) in query.iter_mut() {
+        let airplane_location = transform.translation;
+
+        if airplane_location != airport_location {
+            continue;
+        }
+
+        if tag == &airport_tag {
             commands.entity(entity).despawn_recursive();
 
             score.increment();
@@ -24,6 +31,22 @@ pub fn despawn_airplane(
                 .sender()
                 .send(Event::AirplaneLanded(airplane_id.clone()))
                 .expect("failed to send event"); // TODO: Handle error
+        } else {
+            let go_around = go_around_procedure(map.airport());
+
+            *flight_plan = FlightPlan::new(vec![go_around]);
+
+            commands.entity(entity).remove::<Landing>();
         }
     }
+}
+
+fn go_around_procedure(airport: &Airport) -> Node {
+    let runway_direction = airport.runway().to_vec3();
+    let next_hop_in_direction = runway_direction * Vec3::splat(-2.0);
+
+    Node::new(
+        next_hop_in_direction.x as i32,
+        next_hop_in_direction.y as i32,
+    )
 }
