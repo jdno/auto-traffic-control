72305da3fcd0252bbc1681ff60db942e8d4a3f51
Jan David <jdno@jdno.dev>
2022-04-11 10:55:47 +0200

    Restyle the main screen (#70)
    The main screen that is shown when the game starts has been restyled.
    The text has been replaced by sprites that match the current branding
    of the game.

--- game/src/scene/main_menu.rs
+++ game/src/scene/main_menu.rs
@@ -1,9 +1,7 @@
 use bevy::prelude::*;
 
-use crate::rendering::FONT_COLOR;
+use crate::rendering::RenderLayer;
 use crate::{setup_landscape, AppState};
-
-struct Menu(Entity);
 
 pub struct MainMenuPlugin;
 
@@ -20,53 +18,20 @@ impl Plugin for MainMenuPlugin {
 }
 
 fn spawn(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let text = commands
-        .spawn_bundle(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::Center,
-                flex_direction: FlexDirection::ColumnReverse,
-                ..Default::default()
-            },
-            color: Color::NONE.into(),
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            parent.spawn_bundle(TextBundle {
-                text: Text::with_section(
-                    "Ready",
-                    TextStyle {
-                        font: asset_server.load("font/JetBrainsMono-Regular.ttf"),
-                        font_size: 48.0,
-                        color: FONT_COLOR,
-                    },
-                    Default::default(),
-                ),
-                ..Default::default()
-            });
-            parent.spawn_bundle(TextBundle {
-                style: Style {
-                    margin: Rect::all(Val::Px(24.0)),
-                    ..Default::default()
-                },
-                text: Text::with_section(
-                    "Send a StartGame request to start a game",
-                    TextStyle {
-                        font: asset_server.load("font/JetBrainsMono-Regular.ttf"),
-                        font_size: 24.0,
-                        color: FONT_COLOR,
-                    },
-                    Default::default(),
-                ),
-                ..Default::default()
-            });
-        })
-        .id();
-
-    commands.insert_resource(Menu(text));
+    commands.spawn_bundle(SpriteBundle {
+        transform: Transform::from_xyz(0.0, 0.0, RenderLayer::Ui.z()),
+        texture: asset_server.load("sprites/logo.png"),
+        ..Default::default()
+    });
+    commands.spawn_bundle(SpriteBundle {
+        transform: Transform::from_xyz(0.0, -128.0, RenderLayer::Ui.z()),
+        texture: asset_server.load("sprites/instructions.png"),
+        ..Default::default()
+    });
 }
 
-fn despawn(mut commands: Commands, menu: Res<Menu>) {
-    commands.entity(menu.0).despawn_recursive();
+fn despawn(mut commands: Commands, query: Query<Entity, Without<Camera>>) {
+    for entity in query.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
 }
