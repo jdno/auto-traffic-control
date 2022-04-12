205b53ebc0616861af44b52c15f7fb7533dfdc6d
Jan David <jdno@jdno.dev>
2022-04-12 11:49:36 +0200

    Flatten hierarchy of Node package

--- sdk/node/src/index.ts
+++ sdk/node/src/index.ts
@@ -1,1 +1,11 @@
-export * as v1 from "./v1";
+export * from "./atc/v1/airplane_pb";
+export * from "./atc/v1/airplane_grpc_pb";
+export * from "./atc/v1/atc_pb";
+export * from "./atc/v1/atc_grpc_pb";
+export * from "./atc/v1/event_pb";
+export * from "./atc/v1/event_grpc_pb";
+export * from "./atc/v1/game_pb";
+export * from "./atc/v1/game_grpc_pb";
+export * from "./atc/v1/map_pb";
+export * from "./atc/v1/map_grpc_pb";
+export * from "./atc/v1/tag_pb";
