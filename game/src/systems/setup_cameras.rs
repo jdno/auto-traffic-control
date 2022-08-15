c1b1b2eb52cca3ea710e73a7581fdcaff20e6bdb
Jan David <jdno@jdno.dev>
2022-08-15 13:54:15 +0200

    Update Bevy APIs

--- game/src/systems/setup_cameras.rs
+++ game/src/systems/setup_cameras.rs
@@ -1,6 +1,5 @@
 use bevy::prelude::*;
 
 pub fn setup_cameras(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(UiCameraBundle::default());
+    commands.spawn_bundle(Camera2dBundle::default());
 }
