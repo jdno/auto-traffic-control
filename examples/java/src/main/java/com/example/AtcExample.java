c5abb431732a1d192682e5dba1eac7c5bb796bfc
Jan David <jdno@jdno.dev>
2022-08-23 15:48:42 +0200

    Create test for Java SDK (#180)
    A test case has been added to the GitHub workflow for pull requests that
    builds and then uses the Java SDK. Similar to the other languages, the
    test spawns the test server and prints the version number.

--- examples/java/src/main/java/com/example/AtcExample.java
+++ examples/java/src/main/java/com/example/AtcExample.java
@@ -1,129 +1,85 @@
-
-/* The authors of auto-traffic-control release this file under either Apache v2.0 or MIT license terms. */
-
 package com.example;
 
-
-import atc.v1.Game;
-import atc.v1.Game.StartGameRequest;
-import atc.v1.Game.StartGameResponse;
-import atc.v1.GameServiceGrpc;
-import atc.v1.GameServiceGrpc.GameServiceBlockingStub;
-import atc.v1.GameServiceGrpc.GameServiceFutureStub;
-
+import atc.v1.Atc.GetVersionRequest;
+import atc.v1.Atc.GetVersionResponse;
+import atc.v1.Atc.Version;
+import atc.v1.AtcServiceGrpc;
+import atc.v1.AtcServiceGrpc.AtcServiceBlockingStub;
+import atc.v1.AtcServiceGrpc.AtcServiceFutureStub;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
-
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Future;
-
 
 /**
-Demonstrates connecting and then starting Auto-Traffic-Control.
-*/
-public class AtcExample
-{
+ * Demonstrates connecting and then starting Auto-Traffic-Control.
+ */
+public class AtcExample {
 
-	/** Connects to localhost port 4747 and starts game */
-	public static void main(
-			String[] args
-	) {
-		AtcExample game = new AtcExample( "localhost", 4747 );
-		boolean whetherAsynchronous = true;
-		game.startGame( whetherAsynchronous );
-	}
+    /**
+     * Connects to localhost port 4747 and starts game
+     */
+    public static void main(String[] args) {
+        AtcExample game = new AtcExample("localhost", 4747);
+        boolean whetherAsynchronous = true;
+        game.getVersion(whetherAsynchronous);
+    }
 
+    private Channel channel;
+    private AtcServiceBlockingStub syncingAtcServiceStub;
+    private AtcServiceFutureStub asyncAtcServiceStub;
 
-	private Channel channel;
-	private GameServiceBlockingStub syncingGameServiceStub;
-	private GameServiceFutureStub asyncGameServiceStub;
+    public AtcExample(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+    }
 
+    public AtcExample(ManagedChannelBuilder<?> channelBuilder) {
+        channel = channelBuilder.build();
+        syncingAtcServiceStub = AtcServiceGrpc.newBlockingStub(channel);
+        asyncAtcServiceStub = AtcServiceGrpc.newFutureStub(channel);
+    }
 
-	public AtcExample(
-			String host, int port
-	) {
-		this( ManagedChannelBuilder.forAddress( host, port ).usePlaintext() );
-	}
+    public boolean getVersion(boolean asynchronous) {
+        GetVersionRequest versionRequest = GetVersionRequest.newBuilder().build();
 
+        if (asynchronous) {
+            ListenableFuture<GetVersionResponse> maybeVersion = asyncAtcServiceStub.getVersion(versionRequest);
 
-	public AtcExample(
-			ManagedChannelBuilder<?> channelBuilder
-	) {
-		channel = channelBuilder.build();
-		syncingGameServiceStub = GameServiceGrpc.newBlockingStub( channel );
-		asyncGameServiceStub = GameServiceGrpc.newFutureStub( channel );
-	}
+            int ticksToWait = 1;
+            final CountDownLatch waitForFuture = new CountDownLatch(ticksToWait);
 
+            Futures.addCallback(maybeVersion, new FutureCallback<GetVersionResponse>() {
+                @Override
+                public void onSuccess(GetVersionResponse result) {
+                    printVersion(result.getVersion());
+                    waitForFuture.countDown();
+                }
 
-	public boolean startGame(
-			boolean asynchronous
-	) {
-		final String successMessage = "The game is afoot";
-		StartGameRequest startsGame = StartGameRequest.newBuilder().build();
-		if ( asynchronous )
-		{
-			int ticksToWait = 1;
-			ListenableFuture<StartGameResponse> maybeStarted
-					= asyncGameServiceStub.startGame( startsGame );
-			final CountDownLatch waitForFuture = new CountDownLatch( ticksToWait );
-			Futures.addCallback(
-				maybeStarted,
-				new FutureCallback<StartGameResponse>() {
-					@Override
-					public void onSuccess( StartGameResponse result )
-					{
-						System.out.println( successMessage +" "+ result );
-						waitForFuture.countDown();
-					}
-					//
-					@Override
-					public void onFailure(Throwable t)
-					{
-						throw new Error( t );
-					}
-				},
-				MoreExecutors.directExecutor() );
-			try
-			{
-				waitForFuture.await();
-			}
-			catch ( InterruptedException ie )
-			{
-				ie.printStackTrace();
-				return false;
-			}
-			return true; // ¶ not actually finished, given the asynchronous communication
-		}
-		else
-		{
-			syncingGameServiceStub.startGame( startsGame );
-			System.out.println( successMessage );
-			return true;
-		}
-	}
+                @Override
+                public void onFailure(Throwable t) {
+                    throw new Error(t);
+                }
+            }, MoreExecutors.directExecutor());
+            try {
+                waitForFuture.await();
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+                return false;
+            }
+            return true; // not actually finished, given the asynchronous communication
+        } else {
+            GetVersionResponse result = syncingAtcServiceStub.getVersion(versionRequest);
+            printVersion(result.getVersion());
 
+            return true;
+        }
+    }
 
+    private static void printVersion(Version version) {
+        System.out.println("Auto Traffic Control is running version " + version.getMajor() + "." + version.getMinor() + "." + version.getPatch());
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- sdk/java/README.md
+++ sdk/java/README.md
@@ -1,47 +1,76 @@
-
-<!-- The authors of auto-traffic-control release this file under either Apache v2.0 or MIT license terms. -->
-
-# 🛬 Auto Traffic Control SDK - Java
+# 🛬 Auto Traffic Control
 
 [Homepage](https://auto-traffic-control.com) |
 [Documentation](https://auto-traffic-control.com/docs) |
 [Community](https://github.com/jdno/auto-traffic-control/discussions)
 
-This project generates the Java SDK for the Auto Traffic Control game's gRPC API.
-Until the maintainers of Auto Traffic Control publish official binaries to maven central, you can use this gradle script and the api proto files to generate a jar with grpc.
+**Auto Traffic Control** is a video game played by programming. The player's
+task is to create a program that can safely manage the airspace above two
+airports.
+
+The game is designed to provide an open-ended sandbox that players can use to
+freely practice programming. The game provides a language-agnostic gRPC API,
+giving players free choice of programming language or paradigm.
+
+This project generates the Java SDK for the game's gRPC API.
 
 ## Usage
 
-This sdk utilizes gradle - v7.0 or later - to drive the protobuf compiler.
+This SDK utilizes gradle - v7.0 or later - to drive the protobuf compiler.
 
-. `cd sdk/java`
-. `gradle clean build publishToMavenLocal`
-
-You can now add auto-traffic-control-sdk-java as a dependency in your maven or gradle script:
-
+```shell
+cd sdk/java
+gradle clean build publishToMavenLocal
 ```
+
+You can now add `auto-traffic-control` as a dependency in Gradle...
+
+<!-- markdownlint-disable line-length -->
+
+```groovy
 // gradle terse
-implementation 'dev.jdno.atc:auto-traffic-control-sdk-java:0.3.2
+implementation 'com.auto-traffic-control:auto-traffic-control:0.3.2'
 
 // gradle verbose
-implementation group:'dev.jdno.atc' name: 'auto-traffic-control-sdk-java' version: '0.3.2'
-
-// maven
-<dependency>
-	<groupId>
-		dev.jdno.atc
-		</groupId>
-	<artifactId>
-		auto-traffic-control-sdk-java
-		</artifactId>
-	<version>
-		0.3.2
-		</version>
-	</dependency>
+implementation group: 'com.auto-traffic-control' name : 'auto-traffic-control' version: '0.3.2'
 ```
 
-Check out the [documentation](https://auto-traffic-control.com) to learn about the different services and their endpoints.
-Consider studying the [example project](../../examples/java/src/main/java/com/example).
+<!-- markdownlint-enable line-length -->
 
+...or Maven.
 
+```xml
+<dependency>
+    <groupId>
+        com.auto-traffic-control
+    </groupId>
+    <artifactId>
+        auto-traffic-control
+    </artifactId>
+    <version>
+        0.3.2
+    </version>
+</dependency>
+```
 
+Check out the [documentation](https://auto-traffic-control.com) to learn about
+the different services and their endpoints, and see the
+[example project](../../examples/java/src/main/java/com/example) for a reference
+implementation.
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE)
+  or <http://www.apache.org/licenses/LICENSE-2.0>)
+- MIT license ([LICENSE-MIT](LICENSE-MIT)
+  or <http://opensource.org/licenses/MIT>)
+
+at your option.
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
