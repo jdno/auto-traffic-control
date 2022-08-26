1478a429191e60a116a390088fd029c5803472f3
Jan David <jdno@jdno.dev>
2022-08-26 12:04:52 +0200

    Fix build warnings with tonic and Rust 1.63.0+
    Rust 1.63.0 introduced a new lint that checks if `PartialEq` is derived
    without also deriving `Eq`. This causes warnings when building the game,
    since tonic does not derive `Eq` for the gRPC message types. The current
    recommendation is to explicitly allow the lint in the module that
    imports the Protocol Buffers.
    
    Read more: https://github.com/hyperium/tonic/issues/1056

--- sdk/rust/src/lib.rs
+++ sdk/rust/src/lib.rs
@@ -1,3 +1,8 @@
+// tonic does not derive `Eq` for the gRPC message types, which causes a warning from Clippy. The
+// current suggestion is to explicitly allow the lint in the module that imports the protos.
+// Read more: https://github.com/hyperium/tonic/issues/1056
+#![allow(clippy::derive_partial_eq_without_eq)]
+
 pub mod v1 {
     tonic::include_proto!("atc.v1");
 }
