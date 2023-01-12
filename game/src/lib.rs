45ebbbf16d790c21a026017ac92a3a1814d0f2c5
Jan David <jdno@jdno.dev>
2023-01-12 17:56:45 +0100

    Reorganize crates
    The previous iteration of the game was implemented in a single crate,
    but for this iteration it makes more sense to break the game into
    individual parts. Namely, the `simulation` crate will implement the
    game's core logic and the `api` crate will provide the API layer. This
    will make it possible to embed the simulation in other applications,
    e.g. to provide a custom user interface for the game.

--- game/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! Auto Traffic Control
-//!
-//! Auto Traffic Control is a video game played by programming. The player's task is to create a
-//! program that can safely manage the airspace above two airports.
-//!
-//! The game is designed to provide an open-ended sandbox that players can use to freely practice
-//! programming. It provides a language-agnostic gRPC API, giving players free choice of
-//! programming language or paradigm.
