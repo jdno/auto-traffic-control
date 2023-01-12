0b52c63eac8b9b14a43d475ca69793112adebd8b
Jan David <jdno@jdno.dev>
2023-01-12 18:04:22 +0100

    Reorganize crates (#253)
    The previous iteration of the game was implemented in a single crate,
    but for this iteration it makes more sense to break the game into
    individual parts. Namely, the `simulation` crate will implement the
    game's core logic and the `api` crate will provide the API layer. This
    will make it possible to embed the simulation in other applications,
    e.g. to provide a custom user interface for the game.

---
+++ game/simulation/src/lib.rs
@@ -0,0 +1,14 @@
+//! Auto Traffic Control
+//!
+//! Auto Traffic Control is a video game played by programming. The player's task is to create a
+//! program that can safely manage the airspace above two airports.
+//!
+//! This crate implements the game's simulation engine. The simulation has the following
+//! responsibilities:
+//!
+//!   - Implement the game's entities and rules
+//!   - Start and stop game sessions
+//!
+//! The simulation is not responsible for the API of the game, and can in fact be run without an API
+//! altogether (e.g. for testing). It communicates with the API through command and event busses,
+//! which are passed to the simulation as arguments.
