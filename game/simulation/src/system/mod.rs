864a176359527b1a6c8229e4e00cf19aab421eda
Jan David <jdno@jdno.dev>
2023-01-15 18:47:52 +0100

    Detect collisions (#273)
    A new system has been added that detects collisions between airplanes.
    Contrary to the previous version, the new implementation simply checks
    the distance between two airplanes and compares it to a predefined
    threshold.

--- game/simulation/src/system/mod.rs
+++ game/simulation/src/system/mod.rs
@@ -1,10 +1,12 @@
 use hecs::World;
 
 pub use self::despawn_airplane::*;
+pub use self::detect_collision::*;
 pub use self::move_airplane::*;
 pub use self::spawn_airplane::*;
 
 mod despawn_airplane;
+mod detect_collision;
 mod move_airplane;
 mod spawn_airplane;
 
